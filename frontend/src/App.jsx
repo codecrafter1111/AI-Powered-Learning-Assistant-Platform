c5110ef8dfa9dca0bed841a5a2dd5a3a068ed655
codecrafter <codecrafter432@gmail.com>
2026-07-14 23:34:06 +0530

    Fix import paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import {BrowserRouter as Router,Routes,Route,Navigate} from "react-router-dom"
+import {HashRouter  as Router,Routes,Route,Navigate} from "react-router-dom"
 import LoginPage from "./pages/Auth/LoginPage"
 import RegisterPage from "./pages/Auth/RegisterPage"
 import NotFoundPage from "./pages/NotFoundPage"
