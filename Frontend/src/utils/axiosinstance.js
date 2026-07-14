8791a552e0675a9445e1c5afee8c314ddb72a30a
codecrafter <codecrafter432@gmail.com>
2026-07-14 17:55:05 +0530

    added base in the vite.config.js

--- Frontend/src/utils/axiosinstance.js
+++ Frontend/src/utils/axiosinstance.js
@@ -10,7 +10,7 @@ const axiosInstance = axios.create({
     }
 })
 
-//Request Interceptor
+//Response Interceptor
 axiosInstance.interceptors.request.use(
     (config)=>{
         const accessToken = localStorage.getItem("token")
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/AI-Powered-Learning-Assistant-Platform/",
   plugins: [
     react(),
     tailwindcss(),
